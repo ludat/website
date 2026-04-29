eb5e5a2ed9654aa2bb2add54eaa6c53c8edc4b69
Lucas David Traverso <lucas6246@gmail.com>
2026-04-29 17:13:30 -0300

    Replace tabs with spaces

--- src/pages/[locale]/rss.xml.js
+++ src/pages/[locale]/rss.xml.js
@@ -3,19 +3,19 @@ import rss from '@astrojs/rss';
 import { languages, ui } from '../../i18n/ui';
 
 export function getStaticPaths() {
-	return Object.keys(languages).map((locale) => ({ params: { locale } }));
+  return Object.keys(languages).map((locale) => ({ params: { locale } }));
 }
 
 export async function GET(context) {
-	const locale = context.params.locale;
-	const posts = (await getCollection('blog')).filter((p) => p.id.endsWith(`/${locale}`));
-	return rss({
-		title: ui[locale]['site.title'],
-		description: ui[locale]['site.description'],
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/${locale}/blog/${post.data.urlSlug}/`,
-		})),
-	});
+  const locale = context.params.locale;
+  const posts = (await getCollection('blog')).filter((p) => p.id.endsWith(`/${locale}`));
+  return rss({
+    title: ui[locale]['site.title'],
+    description: ui[locale]['site.description'],
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/${locale}/blog/${post.data.urlSlug}/`,
+    })),
+  });
 }
